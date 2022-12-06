493821c2f6cd4d8c50e1ef41602e20a5c779890d
Tom Heffels <tomheffels@hotmail.com>
2022-12-06 13:03:37 +0100

    Day 6

--- day-6/day6.js
+++ day-6/day6.js
@@ -6,21 +6,18 @@ console.log('SOLUTION PART 1: ', getMarker(input, 4));
 console.log('SOLUTION PART 2: ', getMarker(input, 14));
 
 function getMarker(input, markerLength) {
-  let chars = [];
-  let count = 0;
+  let i,
+    chars = [];
 
-  for (let i = 0; chars.length < markerLength; i++) {
+  for (i = 0; chars.length < markerLength; i++) {
     const char = input[i];
-    const x = markerLength - 1;
-    const lastX = chars.slice(-x);
-    const charIndex = lastX.indexOf(char);
+    const charIndex = chars.indexOf(char);
     const uniqueChar = charIndex === -1;
 
     if (!uniqueChar) chars = chars.slice(charIndex + 1);
 
     chars.push(char);
-    count++;
   }
 
-  return count;
+  return i;
 }
